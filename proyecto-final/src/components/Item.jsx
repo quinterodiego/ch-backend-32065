d1c391b80da8b00e9ac0a99eae93956b0d97b6fe
Diego Quintero <d86webs@gmail.com>
2022-08-01 18:11:18 -0300

    asd

--- proyecto-final/src/components/Item.jsx
+++ proyecto-final/src/components/Item.jsx
@@ -6,12 +6,12 @@ export const Item = ({product}) => {
     return (
         <div className="card m-2" style={divStyle}>
             <img src={product.thumbnail} className="card-img-top" alt="..." />
-            <div className="card-body bg-secondary text-white">
-                <h5 className="card-title">{product.name}</h5>
+            <div className="card-body bg-secondary text-white d-flex flex-column justify-content-between">
+                <p className="card-title h5">{product.name}</p>
                 {/* <p className="card-text">{product.description}</p> */}
-                <div>
-                    <p className="h5">${new Intl.NumberFormat('es-ES').format(product.offers.price)}</p>
-                    <a href="#" className="btn btn-primary">Comprar</a>
+                <div className="d-flex justify-content-between align-items-center">
+                    <p className="h5 m-0 text-dark bg-white p-2">${new Intl.NumberFormat('es-ES').format(product.offers.price)}</p>
+                    <a href="#" className="btn btn-primary"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
