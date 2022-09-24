b27c972c6ca4a2eb8f5f5ef2a47f44812873c575
quinterodiego <d86webs@gmail.com>
2022-09-23 23:25:36 -0300

    asd

--- Routers/carrito.js
+++ Routers/carrito.js
@@ -6,7 +6,7 @@ const routerCarrito = Router();
 
 routerCarrito.get('/:id/productos', async (req, res) => {
     const id = req.params.id;
-    const carrito = await contenedor.getById(id);
+    const carrito = await carritosApi.getById(id);
     if (carrito === null ) {
         res.send({ error: 'Carrito no encontrado' });
     } else {
@@ -16,7 +16,7 @@ routerCarrito.get('/:id/productos', async (req, res) => {
 
 routerCarrito.post('/', async (req, res) => {
     const carrito = req.body;
-    const id = await contenedor.save(carrito);
+    const id = await carritosApi.save(carrito);
     res.send({
         message: `Carrito creado id: ${id}`
     })
@@ -25,20 +25,20 @@ routerCarrito.post('/', async (req, res) => {
 routerCarrito.post('/:id/productos', async (req, res) => {
     const id = req.params.id;
     const producto = req.body;
-    await contenedor.saveProduct(id, producto)
+    await carritosApi.saveProduct(id, producto)
     res.send({message: 'Producto agregado al carrito'});
 });
 
 routerCarrito.delete('/:id', async (req, res) => {
     const id = req.params.id;
-    await contenedor.deleteByCartId(id);
+    await carritosApi.deleteByCartId(id);
     res.send({message: 'Carrito eliminado'});
 });
 
 routerCarrito.delete('/:id/productos/:id_prod', async (req, res) => {
     const id = req.params.id;
     const id_prod = req.params.id_prod;
-    await contenedor.deleteByProductId(id, id_prod);
+    await carritosApi.deleteByProductId(id, id_prod);
     res.send({message: 'Producto eliminado'});
 });
 
